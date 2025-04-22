696a19011b232a970d6cb95b85aea55bddd9c79b
GizemTanis15 <gizemtanis@outlook.com>
2025-04-23 02:44:10 +0300

    Updated app.py with working version before deployment

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ class Ticket(db.Model):
     __tablename__ = 'tickets'
     id = db.Column(db.Integer, primary_key=True)
     ticket_number = db.Column(db.String(10), nullable=False)  
-    flight_id = db.Column(db.Integer, db.ForeignKey('flight.id'), nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'), nullable=False)
     passenger_name = db.Column(db.String(100), nullable=False)
 
 class Checkin(db.Model):
